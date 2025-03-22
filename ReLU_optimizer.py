7cecc28c239055ba14aaed2a70d9b2aafc765bd9
John Miller <millerjo2@grinnell.edu>
2025-03-22 02:52:52 -0700

    reformatting optimizers

--- ReLU_optimizer.py
+++ ReLU_optimizer.py
@@ -7,12 +7,8 @@ class ReLU_optimizer:
     def __init__(self):
         pass
 
-    def backpropagate(
-        self,
-        networkObject: NeuralNetwork,
-        activations: List[List[float]],
-        true_Label: float,
-    ):
+    def backpropagate(self, networkObject: NeuralNetwork, activations: List[List[float]], true_Label: float):
+        
         previous_deltas = []
         learning_rate = 0.01
 
@@ -24,73 +20,46 @@ class ReLU_optimizer:
                     0
                 ]  # the output of the last neuron
 
-                output_node_delta = (true_Label - predicted_value) * ReLU_derivative(
-                    networkObject.network[layer_index][0].z
-                )
+                output_node_delta = (true_Label - predicted_value) * ReLU_derivative(networkObject.network[layer_index][0].z)
 
-                for weight_index in range(
-                    0, len(networkObject.network[layer_index][0].weights)
-                ):
+                for weight_index in range(0, len(networkObject.network[layer_index][0].weights)):
                     previous_weight = networkObject.network[layer_index][0].weights[
                         weight_index
                     ]
-                    new_weight = previous_weight + (
-                        output_node_delta
-                        * learning_rate
-                        * activations[layer_index - 1][weight_index]
-                    )
-                    networkObject.network[layer_index][0].weights[
-                        weight_index
-                    ] = new_weight
+                    new_weight = previous_weight + (output_node_delta* learning_rate * activations[layer_index - 1][weight_index])
+                    networkObject.network[layer_index][0].weights[weight_index] = new_weight
 
                 # update the bias
-                networkObject.network[layer_index][0].bias = networkObject.network[
-                    layer_index
-                ][0].bias + (output_node_delta * learning_rate)
+                networkObject.network[layer_index][0].bias = networkObject.network[layer_index][0].bias + (output_node_delta * learning_rate)
 
                 previous_deltas.append(output_node_delta)
 
             else:
+                 # if we are at a node in a hidden layer, we must now use a different formula to apply the chain rule to the
+                # network to determine the gradients with respect to the weights on the neurons in the current hidden layer
                 new_deltas = []
 
+                
+                # firstly, use a for loop to determine the error signal, that is, sum the weight of each
+                # node in the next layer that connects it to the current node times it's previous delta.
                 for node_index in range(0, len(networkObject.network[layer_index])):
                     error_signal = 0.0
                     for i in range(0, len(networkObject.network[layer_index + 1])):
-                        error_signal += (
-                            networkObject.network[layer_index + 1][i].weights[
-                                node_index
-                            ]
-                            * previous_deltas[i]
-                        )
+                        error_signal += (networkObject.network[layer_index + 1][i].weights[node_index]* previous_deltas[i])
+                    
+                    # use the error signal to determine the current delta
+                    current_delta = ReLU_derivative(networkObject.network[layer_index][node_index].z) * error_signal
 
-                    current_delta = (
-                        ReLU_derivative(
-                            networkObject.network[layer_index][node_index].z
-                        )
-                        * error_signal
-                    )
+                    # for each weight in the current node, use the current delta to update the weights of it's respective node
 
-                    for weight_index in range(
-                        0, len(networkObject.network[layer_index][node_index].weights)
-                    ):
-                        old_weight = networkObject.network[layer_index][
-                            node_index
-                        ].weights[weight_index]
-                        new_weight = old_weight + (
-                            learning_rate
-                            * current_delta
-                            * activations[layer_index - 1][weight_index]
-                        )
-                        networkObject.network[layer_index][node_index].weights[
-                            weight_index
-                        ] = new_weight
+                    for weight_index in range(0, len(networkObject.network[layer_index][node_index].weights)):
+
+                        old_weight = networkObject.network[layer_index][node_index].weights[weight_index]
+                        new_weight = old_weight + (learning_rate * current_delta * activations[layer_index - 1][weight_index])
+                        networkObject.network[layer_index][node_index].weights[weight_index] = new_weight
 
                     # update the bias
-                    networkObject.network[layer_index][
-                        node_index
-                    ].bias = networkObject.network[layer_index][node_index].bias + (
-                        current_delta * learning_rate
-                    )
+                    networkObject.network[layer_index][node_index].bias = networkObject.network[layer_index][node_index].bias + (current_delta * learning_rate)
 
                     new_deltas.append(current_delta)
 
--- sigmoid_optimizer.py
+++ sigmoid_optimizer.py
@@ -9,12 +9,8 @@ class sigmoid_optimizer:
 
     # backpropagate takes the network object, and the activations from each layer, and then uses these to calculate the
     # partial derivatives with respect to each weight. It has a set learning rate of 0.5/search
-    def backpropagate(
-        self,
-        networkObject: NeuralNetwork,
-        activations: List[List[float]],
-        true_Label: float,
-    ):
+    def backpropagate(self, networkObject: NeuralNetwork, activations: List[List[float]], true_Label: float):
+        
         previous_deltas = []
         learning_rate = 0.2
 
@@ -28,34 +24,17 @@ class sigmoid_optimizer:
             if layer_index == len(networkObject.network) - 1:
                 # the delta of the output node for the sigmoid function with respect to the output
                 # node is 2 * label - prediction * (prediction * (1 - prediction))
-                output_node_delta = (
-                    2
-                    * (true_Label - predicted_value)
-                    * predicted_value
-                    * (1 - predicted_value)
-                )
+                output_node_delta = (2 * (true_Label - predicted_value) * predicted_value * (1 - predicted_value))
 
-                for weight_index in range(
-                    0, len(networkObject.network[layer_index][0].weights)
-                ):
+                for weight_index in range(0, len(networkObject.network[layer_index][0].weights)):
                     # for each weight in the output node, use the delta to adjust the weight with the formula:
                     # new weight = old weight + (delta * learning rate * input from the previous layer to the current node)
-                    previous_weight = networkObject.network[layer_index][0].weights[
-                        weight_index
-                    ]
-                    new_weight = previous_weight + (
-                        output_node_delta
-                        * learning_rate
-                        * activations[layer_index - 1][weight_index]
-                    )
-                    networkObject.network[layer_index][0].weights[
-                        weight_index
-                    ] = new_weight
+                    previous_weight = networkObject.network[layer_index][0].weights[weight_index]
+                    new_weight = previous_weight + (output_node_delta * learning_rate * activations[layer_index - 1][weight_index])
+                    networkObject.network[layer_index][0].weights[weight_index] = new_weight
 
                 # update the bias
-                networkObject.network[layer_index][0].bias = networkObject.network[
-                    layer_index
-                ][0].bias + (output_node_delta * learning_rate)
+                networkObject.network[layer_index][0].bias = networkObject.network[layer_index][0].bias + (output_node_delta * learning_rate)
 
                 previous_deltas.append(output_node_delta)
 
@@ -69,41 +48,22 @@ class sigmoid_optimizer:
                     # node in the next layer that connects it to the current node times it's previous delta.
                     error_signal = 0.0
                     for i in range(0, len(networkObject.network[layer_index + 1])):
-                        error_signal += (
-                            networkObject.network[layer_index + 1][i].weights[
-                                node_index
-                            ]
-                            * previous_deltas[i]
-                        )
+                        error_signal += (networkObject.network[layer_index + 1][i].weights[node_index]* previous_deltas[i])
+                        
 
                     # use the error signal to determine the current delta
-                    current_delta = (
-                        activations[layer_index][node_index]
-                        * (1 - activations[layer_index][node_index])
-                    ) * error_signal
+                    current_delta = (activations[layer_index][node_index]* (1 - activations[layer_index][node_index])) * error_signal
 
                     # for each weight in the current node, use the current delta to update the weights of it's respective node
                     for weight_index in range(
-                        0, len(networkObject.network[layer_index][node_index].weights)
-                    ):
-                        old_weight = networkObject.network[layer_index][
-                            node_index
-                        ].weights[weight_index]
-                        new_weight = old_weight + (
-                            learning_rate
-                            * current_delta
-                            * activations[layer_index - 1][weight_index]
-                        )
-                        networkObject.network[layer_index][node_index].weights[
-                            weight_index
-                        ] = new_weight
+                        0, len(networkObject.network[layer_index][node_index].weights)):
+                        old_weight = networkObject.network[layer_index][node_index].weights[weight_index]
+                        new_weight = old_weight + (learning_rate * current_delta * activations[layer_index - 1][weight_index])
+                
+                        networkObject.network[layer_index][node_index].weights[weight_index] = new_weight
 
                     # update the bias
-                    networkObject.network[layer_index][
-                        node_index
-                    ].bias = networkObject.network[layer_index][node_index].bias + (
-                        current_delta * learning_rate
-                    )
+                    networkObject.network[layer_index][node_index].bias = networkObject.network[layer_index][node_index].bias + (current_delta * learning_rate)
 
                     new_deltas.append(current_delta)
 
